27ee49f8312315ccc451ea6d2c09e14f9600b465
borospc <borosgergo@yahoo.com>
2021-10-30 21:04:02 +0200

    team, user, org

---
+++ src/main/java/hu/bme/aut/timechamp/model/Organization.java
@@ -0,0 +1,9 @@
+package hu.bme.aut.timechamp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Organization {
+}
---
+++ src/main/java/hu/bme/aut/timechamp/model/Team.java
@@ -0,0 +1,10 @@
+package hu.bme.aut.timechamp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Team {
+    private String name;
+}
