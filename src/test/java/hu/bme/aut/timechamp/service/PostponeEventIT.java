195114bfd2618191265136b9b02fcdc58d118af2
borospc <borosgergo@yahoo.com>
2021-11-17 22:03:03 +0100

    PostponeEvent mockito and integration tests

---
+++ src/test/java/hu/bme/aut/timechamp/service/PostponeEventIT.java
@@ -0,0 +1,74 @@
+package hu.bme.aut.timechamp.service;
+
+import hu.bme.aut.timechamp.model.Event;
+import hu.bme.aut.timechamp.repository.EventRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class PostponeEventIT {
+
+    @Autowired
+    PostponeEventService postponeEventService;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @BeforeEach
+    public void init(){
+        eventRepository.deleteAll();
+    }
+
+    @Test
+    public void testPostponeEventByMinute(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        eventRepository.save(event);
+
+        //ACT
+        postponeEventService.postponeByMinute("sampleevent",30);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 17, 22, 0));
+
+    }
+
+    @Test
+    public void testPostponeEventByHour(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        eventRepository.save(event);
+
+        //ACT
+        postponeEventService.postponeByHour("sampleevent",1);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 17, 22, 30));
+
+    }
+
+    @Test
+    public void testPostponeEventByDay(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        eventRepository.save(event);
+
+        //ACT
+        postponeEventService.postponeByDay("sampleevent",1);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 18, 21, 30));
+
+    }
+}
---
+++ src/test/java/hu/bme/aut/timechamp/service/PostponeEventTest.java
@@ -0,0 +1,68 @@
+package hu.bme.aut.timechamp.service;
+
+import hu.bme.aut.timechamp.model.Event;
+import hu.bme.aut.timechamp.repository.EventRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+
+@ExtendWith(MockitoExtension.class)
+public class PostponeEventTest {
+
+    @InjectMocks
+    PostponeEventService postponeEventService;
+
+    @Mock
+    EventRepository eventRepository;
+
+    @Test
+    public void testPostponeEventByMinute(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        when(eventRepository.findByName("sampleevent")).thenReturn(Arrays.asList(event));
+
+        //ACT
+        postponeEventService.postponeByMinute("sampleevent",30);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 17, 22, 0));
+
+    }
+
+    @Test
+    public void testPostponeEventByHour(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        when(eventRepository.findByName("sampleevent")).thenReturn(Arrays.asList(event));
+
+        //ACT
+        postponeEventService.postponeByHour("sampleevent",1);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 17, 22, 30));
+    }
+
+    @Test
+    public void testPostponeEventByDay(){
+        //ARRANGE
+        LocalDateTime time = LocalDateTime.of(2021, Month.NOVEMBER, 17, 21, 30);
+        Event event = new Event("sampleevent",time);
+        when(eventRepository.findByName("sampleevent")).thenReturn(Arrays.asList(event));
+
+        //ACT
+        postponeEventService.postponeByDay("sampleevent",1);
+
+        //ASSERT
+        Assertions.assertEquals(event.getTime(),LocalDateTime.of(2021, Month.NOVEMBER, 18, 21, 30));
+    }
+}
