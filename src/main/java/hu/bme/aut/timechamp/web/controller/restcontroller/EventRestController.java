e570ac123c640773a54d911d541c3eaecd077cd7
kalicsadam <kalicsadam@users.noreply.github.com>
2021-12-11 05:36:20 +0100

    implementing some features of EventRestController

--- src/main/java/hu/bme/aut/timechamp/web/controller/restcontroller/EventRestController.java
+++ src/main/java/hu/bme/aut/timechamp/web/controller/restcontroller/EventRestController.java
@@ -5,11 +5,15 @@ import hu.bme.aut.timechamp.dto.EventDto;
 import hu.bme.aut.timechamp.mapper.EventMapper;
 import hu.bme.aut.timechamp.model.Event;
 import hu.bme.aut.timechamp.repository.EventRepository;
+import hu.bme.aut.timechamp.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/events")
@@ -17,6 +21,9 @@ public class EventRestController {
 
     @Autowired
     EventRepository eventRepository;
+
+    @Autowired
+    EventService eventService;
 
     @Autowired
     EventMapper eventMapper;
@@ -28,8 +35,21 @@ public class EventRestController {
         return eventMapper.eventsToDto(events);
     }
 
+
+    @PutMapping
+    public EventDto createEvent(@RequestParam String name, @RequestParam long teamId, @RequestParam long creatorId) {
+        return eventMapper.eventToDto(eventService.createEvent(name, teamId, creatorId));
+    }
+
+
+    @GetMapping("{id}")
+    public EventDto getEventById(@PathVariable long id) {
+        return eventMapper.eventToDto(eventService.getById(id));
+    }
+
+
     @PostMapping
-    public Event createEvent(@RequestBody Event event){
-        return eventRepository.save(event);
+    public EventDto addParticipant(@RequestParam long id, @RequestParam long appUserId) {
+        return eventMapper.eventToDto(eventService.addParticipant(id, appUserId));
     }
 }
