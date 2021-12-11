c3dbefeebe4ce9b88176aa84865a529a7f04a060
Dragoner7 <16624998+Dragoner7@users.noreply.github.com>
2021-12-11 04:21:11 +0100

    Creating organization using name through REST

--- src/main/java/hu/bme/aut/timechamp/repository/OrganizationRepository.java
+++ src/main/java/hu/bme/aut/timechamp/repository/OrganizationRepository.java
@@ -3,8 +3,10 @@ package hu.bme.aut.timechamp.repository;
 import hu.bme.aut.timechamp.model.Organization;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +21,5 @@ public interface OrganizationRepository extends JpaRepository<Organization, Long
     @EntityGraph(attributePaths = "team.organization")
     @Query("SELECT o FROM Organization o")
     List<Organization> findAllWithTeamOrganization();
-
 
 }
