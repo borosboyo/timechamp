1fa93d013ea2afa92c80de23707b1cb0bc503d5e
borospc <borosgergo@yahoo.com>
2021-12-12 23:29:44 +0100

    TeamController parameters

--- src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/TeamController.java
+++ src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/TeamController.java
@@ -2,6 +2,8 @@ package hu.bme.aut.timechamp.web.controller.normalcontroller;
 
 import hu.bme.aut.timechamp.dto.TeamDto;
 import hu.bme.aut.timechamp.service.TeamService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,17 @@ public class TeamController {
     }
 
     @PostMapping("/createTeam")
-    public String createTeam(TeamDto newTeam){
-        teamService.createTeam(newTeam.getName(),newTeam.getCreator().getId(),newTeam.getOrganization().getId());
+    public String createTeam(TeamParameters teamParameters){
+        teamService.createTeam(teamParameters.getName(),teamParameters.getCreatorId(),teamParameters.getOrganizationId());
         return "redirect:/teams";
+    }
+
+    @Setter
+    @Getter
+    static
+    class TeamParameters {
+        private String name;
+        private long creatorId;
+        private long organizationId;
     }
 }
