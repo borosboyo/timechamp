383695bc1b94aead63f27050a6718d0016757d3f
borospc <borosgergo@yahoo.com>
2021-12-12 16:50:38 +0100

    views created

--- src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
+++ src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
@@ -2,15 +2,11 @@ package hu.bme.aut.timechamp.web.controller.normalcontroller;
 
 
 import hu.bme.aut.timechamp.dto.PlaceDto;
-import hu.bme.aut.timechamp.model.Place;
-import hu.bme.aut.timechamp.repository.PlaceRepository;
 import hu.bme.aut.timechamp.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
