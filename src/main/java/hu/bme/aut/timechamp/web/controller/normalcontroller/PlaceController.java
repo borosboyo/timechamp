aec040973992d714b72b0cc6c50f5bddd6749abe
Dragoner7 <16624998+Dragoner7@users.noreply.github.com>
2021-12-12 15:53:44 +0100

    Improve Proof of concept

--- src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
+++ src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
@@ -6,9 +6,11 @@ import hu.bme.aut.timechamp.model.Place;
 import hu.bme.aut.timechamp.repository.PlaceRepository;
 import hu.bme.aut.timechamp.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -24,13 +26,13 @@ public class PlaceController {
         List<PlaceDto> places = placeService.findAll();
 
         model.put("places", places);
-        model.put("place", new Place());
+        model.put("newPlace", new PlaceDto());
         return "placePage";
     }
 
     @PostMapping("/createPlace")
-    public String createPlace(Place place){
-        placeService.createPlace(place.getName(), place.getGoogleCode(), place.getLongitude(), place.getLatitude());
+    public String createPlace(PlaceDto newPlace){
+        placeService.createPlace(newPlace.getName(), newPlace.getGoogleCode(), newPlace.getLongitude(), newPlace.getLatitude());
         return "redirect:/places";
     }
 
