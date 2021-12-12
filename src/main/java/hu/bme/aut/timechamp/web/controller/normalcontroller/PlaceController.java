c215bc7619c8c68e94485d8c613801573287676b
Dragoner7 <16624998+Dragoner7@users.noreply.github.com>
2021-12-12 15:43:34 +0100

    Proof of concept, bit messy

--- src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
+++ src/main/java/hu/bme/aut/timechamp/web/controller/normalcontroller/PlaceController.java
@@ -1,8 +1,10 @@
 package hu.bme.aut.timechamp.web.controller.normalcontroller;
 
 
+import hu.bme.aut.timechamp.dto.PlaceDto;
 import hu.bme.aut.timechamp.model.Place;
 import hu.bme.aut.timechamp.repository.PlaceRepository;
+import hu.bme.aut.timechamp.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,21 +17,20 @@ import java.util.Map;
 public class PlaceController {
 
     @Autowired
-    PlaceRepository placeRepository;
+    PlaceService placeService;
 
     @GetMapping("/places")
     public String home(Map<String, Object> model){
-        List<Place> places = placeRepository.findAll();
+        List<PlaceDto> places = placeService.findAll();
 
         model.put("places", places);
         model.put("place", new Place());
-
         return "placePage";
     }
 
     @PostMapping("/createPlace")
     public String createPlace(Place place){
-        placeRepository.save(place);
+        placeService.createPlace(place.getName(), place.getGoogleCode(), place.getLongitude(), place.getLatitude());
         return "redirect:/places";
     }
 
